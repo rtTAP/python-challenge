efd365a6df741cd8ee9a02ed990337651b03bf3a
rtTAP <tammyapowell@hotmail.com>
2024-04-07 20:47:49 +0800

    not sure

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,31 +2,54 @@
 import os
 import csv
 
-#2 columns of data
-
-
+#create variables for calculations
+total_months = 0
+month_of_change = []
+net_change = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999]
+total_net = 0
 
 #find csv file to read
 csvpath = os.path.join("PyBank","Resources","budget_data.csv")
 #open and read csv file
 with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+    #csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile)
 
     #print(csvreader)
 
-#    read header row first
+#read header row first
     csv_header = next(csvreader)
     print(f'CSV Header: {csv_header}')
+    first_row = next(csvreader)
+    total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
 
-#    number of months of data
-for row in csv.reader:
-  
-    row_count = sum(1 for row in ('csv.reader'))
 
 #read each row of data after the header
-#for rows in csvreader:
-       
-#print(rows)
+
+    for row in csvreader:
+      
+     total_months += 1   
+     total_net += int(row[1])
+
+     net_change = int(row[1]) - prev_net
+     prev_net = int(row[1])
+     net_change_list = [net_change]
+     month_of_change += [row[0]]
+
+     
+
+     #greatest_increase = [net_change] >= 
+
+    #greatest_decrease <= [net_change]
+
+
+    print(net_change)
+    print(total_months)
+    print(total_net)
 
 #loop through to find the below calculations
 
@@ -36,11 +59,12 @@ for row in csv.reader:
 
 #   net change in Profit/Loss over the whole period
 
-#       the average of that change
 
+#       the average of that change
+        #def average(Profit/Losses):
 
 #   Date & amount of the greatest increase in profits over the whole period
-
+         
 
 #   Date & amount of the greatest decrease in profits over the whole period
 
